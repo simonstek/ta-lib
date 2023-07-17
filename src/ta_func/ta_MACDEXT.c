f1c99feb8293d6b709c80a287b28ae9781874223
NeeoSoft Ltd <wisebeer@qq.com>
2023-07-17 17:11:26 +0800

    test

--- src/ta_func/ta_MACDEXT.c
+++ src/ta_func/ta_MACDEXT.c
@@ -405,7 +405,7 @@
     * signal calculation is done, all the output
     * will start at the requested 'startIdx'.
     */
-   retCode = FUNCTION_CALL(MA)( slowEMALookback, endIdx,
+   retCode = FUNCTION_CALL(MA)( 0, endIdx - slowEMALookback,
                                 inReal, optInSlowPeriod, optInSlowMAType,
                                 VALUE_HANDLE_OUT(outBegIdx1), VALUE_HANDLE_OUT(outNbElement1), 
 							    slowMABuffer );
@@ -420,7 +420,7 @@
    }
 
    /* Calculate the fast MA. */
-   retCode = FUNCTION_CALL(MA)( fastEMALookback, endIdx,
+   retCode = FUNCTION_CALL(MA)( 0, endIdx - fastEMALookback,
                                 inReal, optInFastPeriod, optInFastMAType,
                                 VALUE_HANDLE_OUT(outBegIdx2), VALUE_HANDLE_OUT(outNbElement2),
 							    fastMABuffer );
